ffbde421b8dfed5d6b4001fca40b347a79b9b0ff
Michael Lee <michaelsunglee@gmail.com>
2015-02-09 22:47:02 -0500

    Hid Parse and Twitter API keys

---
+++ Donner/Donner/AppDelegate.m
@@ -0,0 +1,77 @@
+//
+//  AppDelegate.m
+//  Donner
+//
+//  Created by Michael Lee on 2015-01-26.
+//  Copyright (c) 2015 leeemichael. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import <Fabric/Fabric.h>
+#import <TwitterKit/TwitterKit.h>
+#import <Parse/Parse.h>
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    [Fabric with:@[TwitterKit]];
+    
+    NSDictionary *dictionary = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Keys" ofType:@"plist"]];
+    NSString *applicationId = [dictionary objectForKey:@"parseApplicationId"];
+    NSString *clientKey = [dictionary objectForKey:@"parseClientKey"];
+    NSString *consumerKey = [dictionary objectForKey:@"twitterConsumerKey"];
+    NSString *secretKey = [dictionary objectForKey:@"twitterSecretKey"];
+    //
+    [Parse setApplicationId:applicationId
+                  clientKey:clientKey];
+    [PFTwitterUtils initializeWithConsumerKey:consumerKey
+                               consumerSecret:secretKey];
+    
+    
+    
+    [PFTwitterUtils logInWithBlock:^(PFUser *user, NSError *error) {
+        if (!user) {
+            NSLog(@"Uh oh. The user cancelled the Twitter login.");
+            return;
+        } else if (user.isNew) {
+            NSLog(@"User signed up and logged in with Twitter!");
+        } else {
+            NSLog(@"User logged in with Twitter!");
+        }
+    }];
+    
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+
+@end
